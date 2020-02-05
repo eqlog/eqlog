8a7df9d21a73c2263d147d1fb89e57f377e94679
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2020-02-05 22:48:44 +0100

    More work on rules

--- src/rules.rs
+++ src/rules.rs
@@ -4,22 +4,42 @@ pub trait Sort {
     fn get_name(&self) -> &str;
 }
 
+pub struct SortVar<T: Sort> {
+    name: String,
+    sort: T
+}
+
+pub fn create_sort_var<T: Sort + Default>(name: &str) -> SortVar<T> {
+    SortVar {
+        name: name.to_string(),
+        sort: Default::default()
+    }
+}
+
 pub trait Relation {
     fn get_name(&self) -> &str;
-    fn dom_sorts(&self) -> &[Rc<dyn Sort>];
+    fn dom_sorts(&self) -> &[Box<dyn Sort>];
+}
+
+pub struct AppliedRelation {
+    relation: Box<dyn Relation>,
+    vars: Vec<String>
 }
 
 pub struct Clause {
+    head: AppliedRelation,
+    body: Vec<AppliedRelation>
 }
 
 pub struct PhlTheory {
-    sorts: Vec<Rc<dyn Sort>>,
+    sorts: Vec<Box<dyn Sort>>,
     relations: Vec<Rc<dyn Relation>>,
     clauses: Vec<Clause>,
 }
 
 macro_rules! Sort {
     ($name:ident) => {
+        #[derive(Default)]
         struct $name { }
 
         impl Sort for $name {
@@ -27,15 +47,13 @@ macro_rules! Sort {
                 stringify!($name)
             }
         }
-
-        let $name = Rc::new($name { });
     }
 }
 
 macro_rules! Rel {
-    ($name:ident, $($s:ident),+) => {
+    ($name:ident, $($id:ident, $ty:ident),+) => {
         struct $name {
-            sorts: Vec<Rc<dyn Sort>>
+            sorts: Vec<Box<dyn Sort>>
         }
 
         impl Relation for $name {
@@ -43,39 +61,61 @@ macro_rules! Rel {
                 stringify!($name)
             }
 
-            fn dom_sorts(&self) -> &[Rc<dyn Sort>] {
+            fn dom_sorts(&self) -> &[Box<dyn Sort>] {
                 &self.sorts
             }
         }
 
-        let $name = Rc::new($name { sorts: vec![$($s.clone()),+] });
+        impl Default for $name {
+            fn default() -> Self {
+                Self {
+                    sorts: vec![$(Box::from($ty::default())),+]
+                }
+            }
+        }
+
+        impl $name {
+            fn apply(&self, $($id: &SortVar<$ty>),+) -> AppliedRelation {
+                AppliedRelation {
+                    relation: Box::from($name::default()),
+                    vars: vec![$($id.name.clone()),+]
+                }
+            }
+        }
+
+        let $name = Rc::new($name::default());
     };
 
-    ($name:ident : $s1:ident) => { Rel!($name, $s1) };
-    ($name:ident : $s1:ident * $s2:ident) => { Rel!($name, $s1, $s2) };
-    ($name:ident : $s1:ident * $s2:ident * $s3:ident) => { Rel!($name, $s1, $s2, $s3) };
-    ($name:ident : $s1:ident * $s2:ident * $s3:ident * $s4:ident) => { Rel!($name, $s1, $s2, $s3, s4) };
-    ($name:ident : $s1:ident * $s2:ident * $s3:ident * $s4:ident * $s5:ident) => { Rel!($name, $s1, $s2, $s3, $s4, $s5) };
+    ($name:ident : $s1:ident) => { Rel!($name, a, $s1) };
+    ($name:ident : $s1:ident * $s2:ident) => { Rel!($name, a, $s1, b, $s2) };
+    ($name:ident : $s1:ident * $s2:ident * $s3:ident) => { Rel!($name, a, $s1, b, $s2, c, $s3) };
+    ($name:ident : $s1:ident * $s2:ident * $s3:ident * $s4:ident) => { Rel!($name, a, $s1, b, $s2, c, $s3, d, $s4) };
+    ($name:ident : $s1:ident * $s2:ident * $s3:ident * $s4:ident * $s5:ident) => { Rel!($name, a, $s1, b, $s2, c, $s3, d, $s4, e, $s5) };
 }
 
 macro_rules! Rule {
-    ($hrel:ident($($hvars:ident),+) :- $($rel:ident($($var:ident),+)),+) => {
-        println!("Matched {}", stringify!($($rel),+))
-    }
+    ($($rel:ident($($var:ident),+))&+ -> $hrel:ident($($hvar:ident),+) with $($v:ident) +) => {{
+        $(let $v = create_sort_var(stringify!($v));)+
+        let head = $hrel.apply($(&$hvar),+);
+        let body = vec![$($rel.apply($(&$var),+)),+];
+        Clause { head, body }
+    }}
 }
 
 #[allow(non_snake_case)]
 pub fn get_dptt() -> PhlTheory {
-    Sort!(CtxS);
-    Sort!(CtxMorphS);
-    Sort!(TyS);
-    Sort!(TmS);
-    Rel!(CtxEq : CtxS * CtxS);
-    Rule!(CtxEq(G, D) :- CtxEq(G, D), CtxEq(D, G));
-    Rule!(CtxEq(G, E) :- CtxEq(G, D), CtxEq(D, E));
+    Sort!(Ctx);
+    Sort!(CtxMorph);
+    Sort!(Ty);
+    Sort!(Tm);
+    Rel!(TyCtx : Ty * Ctx);
+    Rel!(CtxEq : Ctx * Ctx);
+    let ty_ctx_functional = Rule!(TyCtx(s, G) & TyCtx(s, D) -> CtxEq(G, D) with s G D);
+    let ctx_eq_sym = Rule!(CtxEq(G, D) -> CtxEq(D, G) with G D);
+    let ctx_eq_trans = Rule!(CtxEq(G, D) & CtxEq(D, E) -> CtxEq(G, E) with G D E);
     PhlTheory {
-        sorts: vec![CtxS, CtxMorphS, TyS, TmS],
+        sorts: vec![Box::new(Ctx {}), Box::new(CtxMorph {}), Box::new(Ty {}), Box::new(Tm {})],
         relations: vec![CtxEq],
-        clauses: vec![]
+        clauses: vec![ty_ctx_functional, ctx_eq_sym, ctx_eq_trans]
     }
 }
