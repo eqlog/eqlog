f3f327db94e72f533024c28326dc63797e646b8b
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2020-02-10 19:09:17 +0100

    Link PHL into Rust build

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
 extern crate lalrpop;
+extern crate cmake;
+use cmake::Config;
 
 fn main() {
     lalrpop::process_root().unwrap();
-}
+
+    let dst = Config::new("phl").build();       
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-lib=static=rhl");   
+} 
