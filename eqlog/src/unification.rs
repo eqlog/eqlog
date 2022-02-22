e928b468712a8e409541eaaa7d17a75ccd68e286
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2022-02-23 00:53:03 +0100

    Use ena approach; get rid of ...Equality`s

--- eqlog/src/unification.rs
+++ eqlog/src/unification.rs
@@ -1,130 +1,73 @@
 use crate::indirect_ast::*;
 use std::collections::HashMap;
 use std::ops::{Index, IndexMut};
-use std::marker::PhantomData;
+use std::mem::swap;
 
 #[derive(Clone, PartialEq, Eq, Debug)]
-pub struct IdValMap<Id, Val>
-where
-    Id : From<usize> + Into<usize> + Copy,
-{
-    table: Vec<Val>,
-    phantom: PhantomData<Id>,
+pub struct TermMap<Value> {
+    ids: Vec<usize>,
+    values: Vec<Value>,
 }
 
-impl<Id, Val> IdValMap<Id, Val>
-where
-    Id: From<usize> + Into<usize> + Copy,
-    Val: Clone,
-{
-    pub fn new(size: usize, initial_value: Val) -> Self {
-        let mut table = Vec::new();
-        table.resize(size, initial_value);
-        IdValMap {
-            table,
-            phantom: PhantomData,
-        }
-    }
-}
-impl<Id, Val> IdValMap<Id, Val>
-where
-    Id: From<usize> + Into<usize> + Copy,
-{
-    pub fn from_table(table: Vec<Val>) -> Self {
-        IdValMap{
-            table,
-            phantom: PhantomData,
-        }
-    }
-    pub fn iter(&self) -> impl Iterator<Item=(Id, &Val)> {
-        self.table.iter().enumerate().map(|(i, v)| (Id::from(i), v))
+impl<Value> Index<Term> for TermMap<Value> {
+    type Output = Value;
+    fn index(&self, tm: Term) -> &Value {
+        let id = self.ids[tm.0];
+        &self.values[id]
     }
 }
 
-impl<Id, Val> Index<Id> for IdValMap<Id, Val>
-where
-    Id: From<usize> + Into<usize> + Copy,
-{
-    type Output = Val;
-    fn index(&self, i: Id) -> &Val {
-        &self.table[i.into()]
+impl<Value> IndexMut<Term> for TermMap<Value> {
+    fn index_mut(&mut self, tm: Term) -> &mut Value {
+        let id = self.ids[tm.0];
+        &mut self.values[id]
     }
 }
 
-impl<Id, Val> IndexMut<Id> for IdValMap<Id, Val>
-where
-    Id : From<usize> + Into<usize> + Copy
-{
-    fn index_mut(&mut self, i: Id) -> &mut Val {
-        &mut self.table[i.into()]
+impl<Value> TermMap<Value> {
+    pub fn map<U>(self, f: impl FnMut(Value) -> U) -> TermMap<U> {
+        let TermMap{ids, values} = self;
+        let values = values.into_iter().map(f).collect();
+        TermMap{ids, values}
     }
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
-pub struct IdIdMap<FromId, ToId>
-where
-    FromId: From<usize> + Into<usize> + Copy,
-    ToId: From<usize> + Into<usize> + Copy,
-{
-    table: Vec<ToId>,
-    range_end: usize,
-    phantom: PhantomData<FromId>,
-}
-
-impl<FromId, ToId> IdIdMap<FromId, ToId>
-where
-    FromId: From<usize> + Into<usize> + Copy,
-    ToId: From<usize> + Into<usize> + Copy,
-{
-    pub fn new(map: IdValMap<FromId, ToId>) -> Self {
-        let range_max: Option<usize> = map.table.iter().copied().map(|id| id.into()).max();
-        let range_end: usize = range_max.map(|max| max + 1).unwrap_or(0);
-        IdIdMap {
-            table: map.table,
-            phantom: PhantomData,
-            range_end
-        }
-    }
-    pub fn len(&self) -> usize {
-        self.table.len()
-    }
-    pub fn range_end(&self) -> usize {
-        self.range_end
-    }
-    pub fn iter<'a>(&'a self) -> impl 'a + Iterator<Item=(FromId, ToId)> {
-        self.table.iter().copied().enumerate().map(|(i, j)| (FromId::from(i), j))
-    }
-}
-
-impl<FromId, ToId> Index<FromId> for IdIdMap<FromId, ToId>
-where
-    FromId: From<usize> + Into<usize> + Copy,
-    ToId: From<usize> + Into<usize> + Copy,
-{
-    type Output = ToId;
-    fn index(&self, i: FromId) -> &ToId {
-        &self.table[i.into()]
-    }
-}
-
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub struct TermUnification<'a> {
+pub struct TermUnification<'a, Payload, UnionFunction> {
     universe: &'a TermUniverse,
     parents: Vec<usize>,
+    payloads: Vec<Option<Payload>>,
+    union: UnionFunction,
 }
 
-impl<'a> TermUnification<'a> {
-    pub fn new(universe: &'a TermUniverse) -> TermUnification<'a> {
+impl<'a, Payload, UnionFunction> TermUnification<'a, Payload, UnionFunction>
+where
+    UnionFunction: Fn(Payload, Payload) -> Payload,
+{
+    pub fn new(
+        universe: &'a TermUniverse,
+        payloads: Vec<Payload>,
+        union: UnionFunction,
+    ) -> Self {
+        assert_eq!(universe.len(), payloads.len());
         TermUnification {
             universe: universe,
             parents: (0 .. universe.len()).collect(),
+            payloads: payloads.into_iter().map(Some).collect(),
+            union,
         }
     }
-    pub fn union(&mut self, lhs: Term, rhs: Term) {
-        let lhs_root = self.root(lhs);
-        let rhs_root = self.root(rhs);
-        self.parents[lhs_root.0] = rhs_root.0;
+
+    // TODO: Find a way to get rid of this by making `root` take &self.
+    pub fn root_const(&self, tm: Term) -> Term {
+        let mut i = tm.0;
+        while i != self.parents[i] {
+            i = self.parents[i];
+        }
+        let root = i;
+        Term(root)
     }
+
     pub fn root(&mut self, tm: Term) -> Term {
         let mut i = tm.0;
         while i != self.parents[i] {
@@ -139,6 +82,29 @@ impl<'a> TermUnification<'a> {
         }
         Term(root)
     }
+
+    pub fn union(&mut self, lhs: Term, rhs: Term) -> &Payload {
+        let lhs_root = self.root(lhs);
+        let rhs_root = self.root(rhs);
+
+        if lhs_root == rhs_root {
+            return self.payloads[rhs_root.0].as_ref().unwrap();
+        }
+
+        let mut lhs_payload = None;
+        swap(&mut self.payloads[lhs_root.0], &mut lhs_payload);
+
+        let mut rhs_payload = None;
+        swap(&mut self.payloads[rhs_root.0], &mut rhs_payload);
+
+        let new_payload = (self.union)(lhs_payload.unwrap(), rhs_payload.unwrap());
+
+        self.parents[lhs_root.0] = rhs_root.0;
+        self.payloads[rhs_root.0] = Some(new_payload);
+
+        self.payloads[rhs_root.0].as_ref().unwrap()
+    }
+
     pub fn congruence_closure(&mut self) {
         let mut vars: HashMap<&str, Term> = HashMap::new();
         for (tm, data) in self.universe.iter_terms() {
@@ -170,23 +136,52 @@ impl<'a> TermUnification<'a> {
             }
         }
     }
-    pub fn tabulate<Id>(&mut self) -> IdIdMap<Term, Id>
-    where
-        Id: From<usize> + Into<usize> + Copy,
-    {
-        let mut table: IdValMap<Term, Id> = IdValMap::new(self.parents.len(), Id::from(usize::MAX));
 
+    pub fn freeze(mut self) -> TermMap<Payload> {
+        let mut ids = vec![usize::MAX; self.universe.len()];
+        let mut values = Vec::new();
         let mut next_id = 0;
-        for (tm, _) in self.universe.iter_terms() {
-            if tm == self.root(tm) {
-                table[tm] = Id::from(next_id);
+
+        for tm in 0 .. self.universe.len() {
+            if self.parents[tm] == tm {
+                let mut val = None;
+                swap(&mut val, &mut self.payloads[tm]);
+
+                debug_assert_eq!(next_id, values.len());
+                ids[tm] = next_id;
+                values.push(val.unwrap());
                 next_id += 1;
             }
         }
-        for (tm, _) in self.universe.iter_terms() {
-            table[tm] = table[self.root(tm)];
+
+        for tm in 0 .. self.universe.len() {
+            ids[tm] = ids[self.root(Term(tm)).0];
         }
-        IdIdMap::new(table)
+
+        TermMap{ids, values}
+    }
+}
+
+impl<'a, Payload, UnionFunction>
+Index<Term> for TermUnification<'a, Payload, UnionFunction>
+where
+    UnionFunction: Fn(Payload, Payload) -> Payload,
+{
+    type Output = Payload;
+    fn index(&self, tm: Term) -> &Payload {
+        let root = self.root_const(tm);
+        self.payloads[root.0].as_ref().unwrap()
+    }
+}
+
+impl<'a, Payload, UnionFunction>
+IndexMut<Term> for TermUnification<'a, Payload, UnionFunction>
+where
+    UnionFunction: Fn(Payload, Payload) -> Payload,
+{
+    fn index_mut(&mut self, tm: Term) -> &mut Payload {
+        let root = self.root(tm);
+        self.payloads[root.0].as_mut().unwrap()
     }
 }
 
@@ -204,13 +199,34 @@ mod tests {
         let t3 = univ.new_term(Wildcard);
         let t4 = univ.new_term(Wildcard);
         let t5 = univ.new_term(Wildcard);
-        let mut unif = TermUnification::new(&univ);
+
+        let mut unif = TermUnification::new(
+            &univ,
+            vec![true, false, true, false, true, false],
+            |lhs, rhs| { !(lhs && rhs) },
+        );
         assert_eq!(unif.root(t0), t0);
+        assert_eq!(unif[t0], true);
         assert_eq!(unif.root(t1), t1);
+        assert_eq!(unif[t1], false);
         assert_eq!(unif.root(t2), t2);
+        assert_eq!(unif[t2], true);
         assert_eq!(unif.root(t3), t3);
+        assert_eq!(unif[t3], false);
         assert_eq!(unif.root(t4), t4);
+        assert_eq!(unif[t4], true);
         assert_eq!(unif.root(t5), t5);
+        assert_eq!(unif[t5], false);
+    }
+
+    fn new_unification<'a>
+    (universe: &'a TermUniverse) ->
+    TermUnification<'a, bool, impl Fn(bool, bool) -> bool> {
+        TermUnification::new(
+            &universe,
+            (0 .. universe.len()).map(|i| i % 2 == 0).collect(),
+            |lhs, rhs| { !(lhs && rhs) },
+        )
     }
 
     #[test]
@@ -224,30 +240,43 @@ mod tests {
         let t4 = univ.new_term(Wildcard);
         let t5 = univ.new_term(Wildcard);
 
-        let mut unif = TermUnification::new(&univ);
+        let mut unif = new_unification(&univ);
         unif.union(t0, t1);
         assert_eq!(unif.root(t0), unif.root(t1));
         assert!(unif.root(t0) == t0 || unif.root(t0) == t1);
+        assert_eq!(unif[t0], true);
+        assert_eq!(unif[t1], true);
 
-        unif = TermUnification::new(&univ);
+        let mut unif = new_unification(&univ);
         unif.union(t0, t1);
         unif.union(t1, t2);
         assert_eq!(unif.root(t0), unif.root(t2));
         assert_eq!(unif.root(t1), unif.root(t2));
+        assert_eq!(unif[t0], false);
+        assert_eq!(unif[t1], false);
 
-        unif = TermUnification::new(&univ);
+        let mut unif = new_unification(&univ);
         unif.union(t0, t1);
         unif.union(t2, t1);
         assert_eq!(unif.root(t2), unif.root(t0));
         assert_eq!(unif.root(t0), unif.root(t1));
+        assert_eq!(unif[t0], false);
+        assert_eq!(unif[t1], false);
+        assert_eq!(unif[t2], false);
 
-        unif = TermUnification::new(&univ);
+        let mut unif = new_unification(&univ);
         unif.union(t0, t1);
         unif.union(t1, t2);
         unif.union(t3, t4);
         unif.union(t4, t5);
         unif.union(t1, t4);
         assert_eq!(unif.root(t2), unif.root(t3));
+        assert_eq!(unif[t0], true);
+        assert_eq!(unif[t1], true);
+        assert_eq!(unif[t2], true);
+        assert_eq!(unif[t3], true);
+        assert_eq!(unif[t4], true);
+        assert_eq!(unif[t5], true);
     }
 
     #[test]
@@ -261,7 +290,7 @@ mod tests {
         let t4 = univ.new_term(Variable("123".to_string()));
         let t5 = univ.new_term(Application("123".to_string(), vec![t0, t2]));
 
-        let mut unif = TermUnification::new(&univ);
+        let mut unif = new_unification(&univ);
         unif.union(t0, t1);
         unif.congruence_closure();
         assert_eq!(unif.root(t0), unif.root(t1));
@@ -286,7 +315,7 @@ mod tests {
         let t9 = univ.new_term(Application("xyz".to_string(), vec![t1, t6]));
         let t10 = univ.new_term(Application("xyz".to_string(), vec![t1, t8]));
 
-        let mut unif = TermUnification::new(&univ);
+        let mut unif = new_unification(&univ);
         unif.union(t0, t1);
         unif.congruence_closure();
         assert_eq!(unif.root(t0), unif.root(t1));
@@ -302,7 +331,7 @@ mod tests {
     }
 
     #[test]
-    fn test_tabulate() {
+    fn test_freeze() {
         use TermData::*;
         let mut univ = TermUniverse::new();
         let t0 = univ.new_term(Wildcard);
@@ -313,12 +342,18 @@ mod tests {
         let t5 = univ.new_term(Wildcard);
         let _ = univ.new_term(Application("g".to_string(), vec![t0, t2]));
 
-        let mut unif = TermUnification::new(&univ);
+        let mut unif = TermUnification::new(&univ, vec![1; univ.len()], |x, y| x + y);
         unif.union(t0, t1);
 
         unif.union(t3, t4);
         unif.union(t4, t5);
 
-        assert_eq!(unif.tabulate(), IdIdMap::new(IdValMap::from_table(vec![0, 0, 1, 2, 2, 2, 3])));
+        let map = unif.freeze();
+        assert_eq!(map[t0], 2);
+        assert_eq!(map[t1], 2);
+        assert_eq!(map[t2], 1);
+        assert_eq!(map[t3], 3);
+        assert_eq!(map[t4], 3);
+        assert_eq!(map[t5], 3);
     }
 }
