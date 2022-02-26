b303e6787e9e96466feabf7cf3b462df8925c499
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2022-02-26 18:25:01 +0100

    Refactor index selection

--- eqlog/src/index_selection.rs
+++ eqlog/src/index_selection.rs
@@ -1,17 +1,19 @@
 use std::collections::{BTreeSet, HashSet, HashMap};
 use crate::flat_ast::*;
+use crate::indirect_ast::*;
 use itertools::Itertools;
 use std::cmp::Ordering;
-use std::iter::{once, repeat};
+use std::iter::once;
+use crate::signature::Signature;
 
 #[derive(Clone, PartialEq, Eq, Debug, Hash)]
-struct RelationIndex {
+struct QuerySpec {
     projections: BTreeSet<usize>,
     diagonals: BTreeSet<BTreeSet<usize>>,
 }
 
-impl PartialOrd<RelationIndex> for RelationIndex {
-    fn partial_cmp(&self, rhs: &RelationIndex) -> Option<Ordering> {
+impl PartialOrd<QuerySpec> for QuerySpec {
+    fn partial_cmp(&self, rhs: &QuerySpec) -> Option<Ordering> {
         use Ordering::*;
         if self.diagonals != rhs.diagonals {
             None
@@ -27,49 +29,51 @@ impl PartialOrd<RelationIndex> for RelationIndex {
     }
 }
 
-fn index_chains(indices: HashSet<RelationIndex>) -> Vec<Vec<RelationIndex>> {
-    let mut indices: Vec<RelationIndex> = indices.into_iter().collect();
-    indices.sort_by_key(|index| index.projections.len());
+fn query_spec_chains(indices: HashSet<QuerySpec>)-> Vec<Vec<QuerySpec>> {
+    let mut specs: Vec<QuerySpec> = indices.into_iter().collect();
+    specs.sort_by_key(|index| index.projections.len());
 
-    let mut chains: Vec<Vec<RelationIndex>> = Vec::new();
-    for index in indices.into_iter() {
-        let compatible_chain = chains.iter_mut().find(|chain| index >= *chain.last().unwrap());
+    let mut chains: Vec<Vec<QuerySpec>> = Vec::new();
+    for spec in specs.into_iter() {
+        let compatible_chain = chains.iter_mut().find(|chain| spec >= *chain.last().unwrap());
         match compatible_chain {
-            Some(compatible_chain) => compatible_chain.push(index),
-            None => chains.push(vec![index]),
+            Some(compatible_chain) => compatible_chain.push(spec),
+            None => chains.push(vec![spec]),
         }
     }
     chains
 }
 
-fn index_chain_to_order(arity: usize, chain: &[RelationIndex]) -> Vec<usize> {
-    let empty_projections = BTreeSet::new();
-    let full_projections: BTreeSet<usize> = (0 .. arity).collect();
-
-    // Some `impl Iterator<&BTreeSet<usize>`:
-    let chain = chain.iter().map(|index| &index.projections);
-    let bot_chain = once(&empty_projections).chain(chain.clone());
-    let top_chain = chain.chain(once(&full_projections));
-    // An `impl Iterator<BTreeSet<usize>`:
-    let diffs = bot_chain.zip(top_chain).map(|(prev, next)| next - prev);
-
-    diffs.flatten().collect()
+#[derive(Clone, PartialEq, Eq, Debug, Hash)]
+struct IndexSpec {
+    order: Vec<usize>,
+    diagonals: BTreeSet<BTreeSet<usize>>,
 }
 
-struct IndexRequirements {
-    relations: HashMap<String, HashSet<RelationIndex>>,
-    sorts: HashMap<String, bool>,
+fn is_prefix(prefix: &BTreeSet<usize>, order: &[usize]) -> bool {
+    let count = order.iter().take_while(|el| prefix.contains(el)).count();
+    count == prefix.len()
 }
 
-impl IndexRequirements {
-    fn new(
-        sorts: impl IntoIterator<Item=String>,
-        relations: impl IntoIterator<Item=String>,
-    ) -> Self {
-        IndexRequirements {
-            relations: relations.into_iter().zip(repeat(HashSet::new())).collect(),
-            sorts: sorts.into_iter().zip(repeat(false)).collect(),
-        }
+impl IndexSpec {
+    pub fn can_serve(&self, query: &QuerySpec) -> bool {
+        query.diagonals == self.diagonals && is_prefix(&query.projections, &self.order)
+    }
+    pub fn from_query_spec_chain(arity_len: usize, chain: &[QuerySpec]) -> Self {
+        let empty_projections = BTreeSet::new();
+        let full_projections: BTreeSet<usize> = (0 .. arity_len).collect();
+
+        // Some `impl Iterator<&BTreeSet<usize>`:
+        let proj_chain = chain.iter().map(|query| &query.projections);
+        let bot_chain = once(&empty_projections).chain(proj_chain.clone());
+        let top_chain = proj_chain.chain(once(&full_projections));
+        // An `impl Iterator<BTreeSet<usize>`:
+        let diffs = bot_chain.zip(top_chain).map(|(prev, next)| next - prev);
+
+        let order: Vec<usize> = diffs.flatten().collect();
+
+        let diagonals = chain.last().unwrap().diagonals.clone();
+        IndexSpec{order, diagonals}
     }
 }
 
@@ -93,36 +97,71 @@ fn projections(fixed_terms: &HashSet<FlatTerm>, args: &[FlatTerm]) -> BTreeSet<u
         .collect()
 }
 
-impl IndexRequirements {
-    fn add_from_premise(&mut self, premise: &[FlatAtom]) {
-        let mut fixed_terms: HashSet<FlatTerm> = HashSet::new();
-        for atom in premise.iter() {
-            use FlatAtom::*;
-            match atom {
-                Equal(lhs, rhs) => {
-                    fixed_terms.insert(*lhs);
-                    fixed_terms.insert(*rhs);
-                },
-                Relation(rel, args) => {
-                    let projections = projections(&fixed_terms, args);
-                    let diagonals = diagonals(args);
+fn query_specs_for_premise<'a>(premise: &'a [FlatAtom]) -> impl 'a + Iterator<Item=(&'a str, QuerySpec)> {
+    let mut fixed_terms: HashSet<FlatTerm> = HashSet::new();
+    let spec_for_atom = move |atom: &'a FlatAtom| -> Option<(&'a str, QuerySpec)> {
+        use FlatAtom::*;
+        match atom {
+            Equal(lhs, rhs) => {
+                fixed_terms.insert(*lhs);
+                fixed_terms.insert(*rhs);
+                None
+            },
+            Relation(rel, args) => {
+                let projections = projections(&fixed_terms, args);
+                let diagonals = diagonals(args);
 
-                    if !projections.is_empty() || !diagonals.is_empty() {
-                        let index = RelationIndex{projections, diagonals};
-                        self.relations.get_mut(rel).unwrap().insert(index);
-                    }
+                for arg in args.iter().copied() {
+                    fixed_terms.insert(arg);
+                }
 
-                    for arg in args.iter().copied() {
-                        fixed_terms.insert(arg);
-                    }
-                },
-                Unconstrained(tm, sort) => {
-                    fixed_terms.insert(*tm);
-                    *self.sorts.get_mut(sort).unwrap() = true;
-                },
-            }
+                if !projections.is_empty() || !diagonals.is_empty() {
+                    Some((rel.as_str(), QuerySpec{projections, diagonals}))
+                } else {
+                    None
+                }
+            },
+            Unconstrained(tm, _) => {
+                fixed_terms.insert(*tm);
+                None
+            },
         }
+    };
+
+    premise.iter().map(spec_for_atom).flatten()
+}
+
+fn index_specs(signature: &Signature, axioms: &[FlatSequent]) -> HashMap<String, Vec<IndexSpec>> {
+    let mut query_specs: HashMap<String, (usize, HashSet<QuerySpec>)> =
+        signature.relations()
+        .map(|(rel, arity)| (rel.to_string(), (arity.len(), HashSet::new())))
+        .collect();
+
+    // Add indices for (implicit) functionality axioms.
+    for func in signature.functions().values() {
+        let spec = QuerySpec {
+            projections: (0 .. func.dom.len()).collect(),
+            diagonals: BTreeSet::new(),
+        };
+        query_specs.get_mut(&func.name).unwrap().1.insert(spec);
     }
+
+    // Add indices for axioms.
+    let rel_specs = axioms.iter().flat_map(|sequent| query_specs_for_premise(&sequent.premise));
+    for (rel, query_spec) in rel_specs {
+        query_specs.get_mut(rel).unwrap().1.insert(query_spec);
+    }
+
+    query_specs.into_iter()
+        .map(|(rel, (arity_len, query_specs))| {
+            let chains = query_spec_chains(query_specs);
+            let index_specs: Vec<IndexSpec> = 
+                chains.iter()
+                .map(|chain| IndexSpec::from_query_spec_chain(arity_len, chain))
+                .collect();
+            (rel, index_specs)
+        })
+        .collect() 
 }
 
 #[cfg(test)]
@@ -131,16 +170,97 @@ mod tests {
 use super::*;
 
 #[test]
+fn test_can_serve() {
+    let query0 = QuerySpec{
+        projections: btreeset!{0, 2, 3},
+        diagonals: btreeset!{},
+    };
+    let query1 = QuerySpec{
+        projections: btreeset!{0, 2, 3, 1},
+        diagonals: btreeset!{},
+    };
+    let query2 = QuerySpec{
+        projections: btreeset!{0, 2},
+        diagonals: btreeset!{btreeset!{0, 3}},
+    };
+    let query3 = QuerySpec{
+        projections: btreeset!{0, 2},
+        diagonals: btreeset!{btreeset!{0, 3}, btreeset!{1, 2}},
+    };
+
+    let index0 = IndexSpec{
+        order: vec![2, 0, 3, 1],
+        diagonals: btreeset!{},
+    };
+    let index1 = IndexSpec{
+        order: vec![2, 0, 1, 3],
+        diagonals: btreeset!{},
+    };
+    let index2 = IndexSpec{
+        order: vec![2, 0, 1, 3],
+        diagonals: btreeset!{btreeset!{0, 3}},
+    };
+    let index3 = IndexSpec{
+        order: vec![0, 3, 1, 2],
+        diagonals: btreeset!{btreeset!{0, 3}, btreeset!{1, 2}},
+    };
+
+    assert!(index0.can_serve(&query0));
+    assert!(!index1.can_serve(&query0));
+    assert!(!index2.can_serve(&query0));
+    assert!(!index3.can_serve(&query0));
+
+    assert!(index0.can_serve(&query1));
+    assert!(index1.can_serve(&query1));
+    assert!(!index2.can_serve(&query1));
+    assert!(!index3.can_serve(&query1));
+
+    assert!(!index0.can_serve(&query2));
+    assert!(!index1.can_serve(&query2));
+    assert!(index2.can_serve(&query2));
+    assert!(!index3.can_serve(&query2));
+
+    assert!(!index0.can_serve(&query3));
+    assert!(!index1.can_serve(&query3));
+    assert!(!index2.can_serve(&query3));
+    assert!(!index3.can_serve(&query3));
+}
+
+fn obj() -> String { "obj".to_string() }
+fn mor() -> String { "mor".to_string() }
+fn comp() -> String { "comp".to_string() }
+fn id() -> String { "id".to_string() }
+fn signature() -> String { "signature".to_string() }
+
+fn category_signature() -> Signature {
+    let mut sig = Signature::new();
+    sig.add_sort(Sort(obj()));
+    sig.add_sort(Sort(mor()));
+    sig.add_function(Function{name: comp(), dom: vec![mor(), mor()], cod: mor()});
+    sig.add_function(Function{name: id(), dom: vec![obj()], cod: mor()});
+    sig.add_predicate(Predicate{name: signature(), arity: vec![obj(), mor(), obj()]});
+    sig
+}
+
+fn category_relations() -> HashSet<String> {
+    hashset!{comp(), id(), signature()}
+}
+
+fn can_serve<'a>(indices: impl IntoIterator<Item=&'a IndexSpec>, query: &QuerySpec) -> bool {
+    indices.into_iter().find(|index| index.can_serve(query)).is_some()
+}
+
+#[test]
 fn simple_reduction() {
     // comp(h, comp(g, f)) ~> comp(comp(h, g), f)";
-    let mor = || "mor".to_string();
-    let comp = || "comp".to_string();
+    let sig = category_signature();
     let h = FlatTerm(0);
     let g = FlatTerm(1);
     let f = FlatTerm(2);
     let gf = FlatTerm(3);
     let hg = FlatTerm(4);
     let hg_f = FlatTerm(5);
+    let h_gf = hg_f;
 
     use FlatAtom::*;
     let premise = vec![
@@ -150,78 +270,93 @@ fn simple_reduction() {
         // comp(comp(h, g), f)!
         Relation(comp(), vec![hg, f, hg_f]),
     ];
+    let conclusion = vec![
+        // comp(h, comp(g, f)) = comp(comp(h, g), f)
+        Relation(comp(), vec![h, gf, h_gf])
+    ];
+    let axioms = vec![FlatSequent{premise, conclusion}];
 
-    let mut indices = IndexRequirements::new([mor()], [comp()]);
-    indices.add_from_premise(&premise);
+    let index_specs = index_specs(&sig, &axioms);
 
-    let relation_indices = hashmap!{
-        comp() => hashset!{
-            RelationIndex {
-                projections: btreeset!{1},
-                diagonals: btreeset!{},
-            },
-            RelationIndex {
-                projections: btreeset!{0, 1},
-                diagonals: btreeset!{},
-            },
-        },
-    };
-    assert_eq!(indices.relations, relation_indices);
+    let rels: HashSet<String> = index_specs.keys().map(|s| s.to_string()).collect();
+    assert_eq!(rels, category_relations());
 
-    let sort_indices = hashmap!{
-        mor() => false,
-    };
-    assert_eq!(indices.sorts, sort_indices);
+    let comp_specs = index_specs.get(&comp()).unwrap();
+    let id_specs = index_specs.get(&id()).unwrap();
+    let signature_specs = index_specs.get(&signature()).unwrap();
+
+    // Second atom.
+    assert!(can_serve(
+            comp_specs.iter(),
+            &QuerySpec{projections: btreeset!{1}, diagonals: btreeset!{}}
+    ));
+    // Third atom.
+    assert!(can_serve(
+            comp_specs.iter(),
+            &QuerySpec{projections: btreeset!{0, 1}, diagonals: btreeset!{}}
+    ));
+    // Functionality.
+    assert!(can_serve(
+            comp_specs.iter(),
+            &QuerySpec{projections: btreeset!{0, 1}, diagonals: btreeset!{}}
+    ));
+    // Functionality.
+    assert!(can_serve(
+            id_specs.iter(),
+            &QuerySpec{projections: btreeset!{0}, diagonals: btreeset!{}}
+    ));
+
+    assert_eq!(comp_specs.len(), 1);
+    assert_eq!(id_specs.len(), 1);
+    assert_eq!(signature_specs.len(), 0);
 }
 
 #[test]
 fn non_surjective_implication() {
     // signature(x, f, y) & signature(y, g, z) => comp(g, f)! & signature(x, comp(g, f), z)
-    let obj = || "obj".to_string();
-    let mor = || "mor".to_string();
-    let signature = || "signature".to_string();
-    let comp = || "comp".to_string();
+    let sig = category_signature();
     let x = FlatTerm(0);
     let f = FlatTerm(1);
     let y = FlatTerm(2);
     let g = FlatTerm(3);
     let z = FlatTerm(4);
+    let gf = FlatTerm(5);
 
     use FlatAtom::*;
     let premise = vec![
         Relation(signature(), vec![x, f, y]),
         Relation(signature(), vec![y, g, z]),
     ];
+    let conclusion = vec![
+        Relation(comp(), vec![g, f, gf]),
+        Relation(signature(), vec![x, gf, z]),
+    ];
+    let axioms = vec![FlatSequent{premise, conclusion}];
 
-    let mut indices = IndexRequirements::new([obj(), mor()], [signature(), comp()]);
-    indices.add_from_premise(&premise);
+    let index_specs = index_specs(&sig, &axioms);
 
-    let relation_indices = hashmap!{
-        signature() => hashset!{
-            RelationIndex {
-                projections: btreeset!{0},
-                diagonals: btreeset!{},
-            },
-        },
-        comp() => hashset!{},
-    };
-    assert_eq!(indices.relations, relation_indices);
+    let rels: HashSet<String> = index_specs.keys().map(|s| s.to_string()).collect();
+    assert_eq!(rels, category_relations());
 
-    let sort_indices = hashmap!{
-        obj() => false,
-        mor() => false,
-    };
-    assert_eq!(indices.sorts, sort_indices);
+    let signature_specs = index_specs.get(&signature()).unwrap();
+    let id_specs = index_specs.get(&id()).unwrap();
+    let comp_specs = index_specs.get(&comp()).unwrap();
+
+    // Second atom.
+    assert!(can_serve(
+            signature_specs.iter(),
+            &QuerySpec{projections: btreeset!{0}, diagonals: btreeset!{}}
+    ));
+    assert_eq!(signature_specs.len(), 1);
+    // Functionality only.
+    assert_eq!(id_specs.len(), 1);
+    assert_eq!(comp_specs.len(), 1);
 }
 
 #[test]
 fn simultaneous_projection_and_diagonal() {
     // i = id(x) & signature(y, i, y) & signature(x, f, x) & j = id(y) => x = y & j = i
-    let obj = || "obj".to_string();
-    let mor = || "mor".to_string();
-    let id = || "id".to_string();
-    let signature = || "signature".to_string();
-
+    let sig = category_signature();
     let i = FlatTerm(0);
     let x = FlatTerm(1);
     let y = FlatTerm(2);
@@ -235,61 +370,39 @@ fn simultaneous_projection_and_diagonal() {
         Relation(signature(), vec![x, f, x]),
         Relation(id(), vec![y, j]),
     ];
-
-    let mut indices = IndexRequirements::new([obj(), mor()], [id(), signature()]);
-    indices.add_from_premise(&premise);
-
-    let relation_indices = hashmap!{
-        signature() => hashset!{
-            RelationIndex {
-                projections: btreeset!{1},
-                diagonals: btreeset!{btreeset!{0, 2}},
-            },
-            RelationIndex {
-                projections: btreeset!{0, 2},
-                diagonals: btreeset!{btreeset!{0, 2}},
-            },
-        },
-        id() => hashset!{
-            RelationIndex {
-                projections: btreeset!{0},
-                diagonals: btreeset!{},
-            },
-        },
-    };
-    assert_eq!(indices.relations, relation_indices);
-
-    let sort_indices = hashmap!{
-        obj() => false,
-        mor() => false,
-    };
-    assert_eq!(indices.sorts, sort_indices);
-}
-
-#[test]
-fn unconstrained_variable() {
-    // !x: obj & !y: obj => x = y
-    let obj = || "obj".to_string();
-    let mor = || "mor".to_string();
-    let x = FlatTerm(0);
-    let y = FlatTerm(1);
-
-    use FlatAtom::*;
-    let premise = vec![
-        Unconstrained(x, obj()),
-        Unconstrained(y, obj()),
+    let conclusion = vec![
+        Equal(x, y),
+        Equal(j, i),
     ];
-    let mut indices = IndexRequirements::new([obj(), mor()], []);
-    indices.add_from_premise(&premise);
+    let axioms = vec![FlatSequent{premise, conclusion}];
 
-    let relation_indices = hashmap!{};
-    assert_eq!(indices.relations, relation_indices);
+    let index_specs = index_specs(&sig, &axioms);
 
-    let sort_indices = hashmap!{
-        obj() => true,
-        mor() => false,
-    };
-    assert_eq!(indices.sorts, sort_indices);
+    let rels: HashSet<String> = index_specs.keys().map(|s| s.to_string()).collect();
+    assert_eq!(rels, category_relations());
+
+    let signature_specs = index_specs.get(&signature()).unwrap();
+    let id_specs = index_specs.get(&id()).unwrap();
+    let comp_specs = index_specs.get(&comp()).unwrap();
+
+    // Second atom.
+    assert!(can_serve(
+            signature_specs.iter(),
+            &QuerySpec{projections: btreeset!{1}, diagonals: btreeset!{btreeset!{0, 2}}}
+    ));
+    // Third atom.
+    assert!(can_serve(
+            signature_specs.iter(),
+            &QuerySpec{projections: btreeset!{0, 2}, diagonals: btreeset!{btreeset!{0, 2}}}
+    ));
+    assert_eq!(signature_specs.len(), 2);
+    // Fourth atom.
+    assert!(can_serve(
+            id_specs.iter(),
+            &QuerySpec{projections: btreeset!{0}, diagonals: btreeset!{}}
+    ));
+    assert_eq!(id_specs.len(), 1);
+    assert_eq!(comp_specs.len(), 1);
 }
 
 }
