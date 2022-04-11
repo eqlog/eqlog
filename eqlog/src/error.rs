d9ba5f18fb9167574873cdab5f5541aa9a17f792
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2022-04-12 01:37:45 +0200

    Add location formatting

--- eqlog/src/error.rs
+++ eqlog/src/error.rs
@@ -79,6 +79,61 @@ impl<'a> From<ParseError<usize, Token<'a>, CompileError>> for CompileError {
     }
 }
 
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
+struct FormatLocation<'a>(&'a str, Location);
+
+impl<'a> Display for FormatLocation<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let FormatLocation(src, Location(begin, end)) = *self;
+
+        let line_ranges = src.lines().scan(0, |len, line| {
+            let b = *len;
+            // TODO: Or line.chars().count()?
+            *len += line.len();
+            let e = *len;
+            // TODO: Doesn't work with \n\r newlines.
+            *len += 1;
+            Some((b, e))
+        });
+
+        let intersecting_line_ranges = line_ranges
+            .enumerate()
+            .skip_while(|(_, (_, e))| *e <= begin)
+            .take_while(|(_, (b, _))| *b < end)
+            .map(|(i, r)| (i + 1, r));
+
+        let max_line_num_digits = itertools::max(intersecting_line_ranges.clone().map(|(i, _)| i))
+            .map(|i| i.to_string().len());
+
+        for (i, (b, e)) in intersecting_line_ranges {
+            let line_num_str = i.to_string();
+            for _ in line_num_str.len()..max_line_num_digits.unwrap() {
+                write!(f, " ")?;
+            }
+            write!(f, "{line_num_str} | ")?;
+            write!(f, "{}\n", &src[b..e])?;
+
+            for _ in 0..max_line_num_digits.unwrap() {
+                write!(f, " ")?;
+            }
+            write!(f, " | ")?;
+            for i in b..e {
+                if i < begin || i >= end {
+                    write!(f, " ")?;
+                } else {
+                    write!(f, "^")?;
+                }
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
+}
+
+fn format_location(source: &str, location: Location) -> impl '_ + Display + Copy {
+    FormatLocation(source, location)
+}
+
 #[derive(Clone, PartialEq, Eq, Hash, Debug)]
 pub struct CompileErrorWithSource {
     pub error: CompileError,
@@ -88,7 +143,8 @@ pub struct CompileErrorWithSource {
 impl Display for CompileErrorWithSource {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use CompileError::*;
-        match &self.error {
+        let CompileErrorWithSource { error, source } = self;
+        match error {
             InvalidToken { location: _ } => {
                 write!(f, "invalid token")?;
             }
@@ -111,12 +167,15 @@ impl Display for CompileErrorWithSource {
                 function: _,
                 expected,
                 got,
-                location: _,
+                location,
             } => {
                 write!(
                     f,
-                    "function takes {expected} arguments but {got} were supplied"
+                    "Function takes {expected} arguments but {got} were supplied\n"
                 )?;
+                if let Some(loc) = *location {
+                    write!(f, "{}", format_location(source, loc))?;
+                }
             }
             PredicateArgumentNumber {
                 predicate: _,
