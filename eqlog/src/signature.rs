337cbf837308d32cacba66fc70896f9ea9584579
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2022-04-13 00:15:43 +0200

    First step towards unified symbol table

--- eqlog/src/signature.rs
+++ eqlog/src/signature.rs
@@ -1,10 +1,54 @@
 use crate::ast::*;
 use crate::error::*;
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
 use std::iter::once;
+
+#[derive(Clone, PartialEq, Eq, Debug, Hash)]
+pub enum Symbol {
+    Sort(Sort),
+    Predicate(Predicate),
+    Function(Function),
+}
+
+impl Symbol {
+    fn name(&self) -> &str {
+        use Symbol::*;
+        match self {
+            Sort(s) => &s.name,
+            Predicate(p) => &p.name,
+            Function(f) => &f.name,
+        }
+    }
+    fn location(&self) -> Option<Location> {
+        match self {
+            Symbol::Sort(Sort { location, .. }) => *location,
+            Symbol::Predicate(Predicate { location, .. }) => *location,
+            Symbol::Function(Function { location, .. }) => *location,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct SymbolUpToName(Symbol);
+
+impl PartialEq for SymbolUpToName {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.name() == other.0.name()
+    }
+}
+impl Eq for SymbolUpToName {}
+
+impl Hash for SymbolUpToName {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.name().hash(state);
+    }
+}
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct Signature {
+    symbols: HashSet<SymbolUpToName>,
     sorts: HashMap<String, Sort>,
     predicates: HashMap<String, Predicate>,
     functions: HashMap<String, Function>,
@@ -13,6 +57,7 @@ pub struct Signature {
 impl Signature {
     pub fn new() -> Self {
         Signature {
+            symbols: HashSet::new(),
             sorts: HashMap::new(),
             predicates: HashMap::new(),
             functions: HashMap::new(),
@@ -53,58 +98,64 @@ impl Signature {
         if let Some(Function { dom, cod, .. }) = self.functions.get(relation) {
             return Some(dom.iter().chain(once(cod)).map(|s| s.as_str()).collect());
         }
-
         None
     }
 
-    pub fn add_sort(&mut self, sort: Sort) -> Result<(), CompileError> {
-        let sort_location = sort.location;
-        if let Some(prev_sort) = self.sorts.insert(sort.name.clone(), sort) {
+    fn insert_symbol(&mut self, symbol: Symbol) -> Result<(), CompileError> {
+        let second_location = symbol.location();
+        if let Some(SymbolUpToName(prev_symbol)) = self.symbols.replace(SymbolUpToName(symbol)) {
             return Err(CompileError::SymbolDeclaredTwice {
-                name: prev_sort.name.clone(),
-                first_declaration: prev_sort.location,
-                second_declaration: sort_location,
+                name: prev_symbol.name().into(),
+                first_declaration: prev_symbol.location(),
+                second_declaration: second_location,
             });
         }
         Ok(())
+    }
+    fn get_symbol(&mut self, name: &str) -> Option<&Symbol> {
+        self.symbols
+            .get(&SymbolUpToName(Symbol::Sort(Sort {
+                name: name.into(),
+                location: None,
+            })))
+            .map(|upto| &upto.0)
+    }
+
+    pub fn add_sort(&mut self, sort: Sort) -> Result<(), CompileError> {
+        self.sorts.insert(sort.name.clone(), sort.clone());
+        self.insert_symbol(Symbol::Sort(sort))
     }
     pub fn add_predicate(&mut self, pred: Predicate) -> Result<(), CompileError> {
+        self.predicates.insert(pred.name.clone(), pred.clone());
         for s in pred.arity.iter() {
-            if !self.sorts.contains_key(s) {
-                return Err(CompileError::UndeclaredSymbol {
-                    name: s.clone(),
-                    location: pred.location,
-                });
+            match self.get_symbol(s) {
+                None => {
+                    return Err(CompileError::UndeclaredSymbol {
+                        name: s.clone(),
+                        location: pred.location,
+                    });
+                }
+                Some(Symbol::Sort { .. }) => (),
+                Some(_) => panic!("Is not sort"),
             }
         }
-        let pred_location = pred.location;
-        if let Some(prev_pred) = self.predicates.insert(pred.name.clone(), pred) {
-            return Err(CompileError::SymbolDeclaredTwice {
-                name: prev_pred.name.clone(),
-                first_declaration: prev_pred.location,
-                second_declaration: pred_location,
-            });
-        }
-        Ok(())
+        self.insert_symbol(Symbol::Predicate(pred))
     }
     pub fn add_function(&mut self, func: Function) -> Result<(), CompileError> {
+        self.functions.insert(func.name.clone(), func.clone());
         for s in func.dom.iter().chain(once(&func.cod)) {
-            if !self.sorts.contains_key(s) {
-                return Err(CompileError::UndeclaredSymbol {
-                    name: s.clone(),
-                    location: func.location,
-                });
+            match self.get_symbol(s) {
+                None => {
+                    return Err(CompileError::UndeclaredSymbol {
+                        name: s.clone(),
+                        location: func.location,
+                    });
+                }
+                Some(Symbol::Sort { .. }) => (),
+                Some(_) => panic!("Is not sort"),
             }
         }
-        let func_location = func.location;
-        if let Some(prev_func) = self.functions.insert(func.name.clone(), func) {
-            return Err(CompileError::SymbolDeclaredTwice {
-                name: prev_func.name.clone(),
-                first_declaration: prev_func.location,
-                second_declaration: func_location,
-            });
-        }
-        Ok(())
+        self.insert_symbol(Symbol::Function(func))
     }
 }
 
@@ -127,7 +178,8 @@ mod tests {
 
         th.add_function(Function::new("F".to_string(), vec![s(), t()], t()))
             .unwrap();
-        th.add_function(Function::new("G".to_string(), vec![t(), s()], t()));
+        th.add_function(Function::new("G".to_string(), vec![t(), s()], t()))
+            .unwrap();
     }
 
     #[test]
