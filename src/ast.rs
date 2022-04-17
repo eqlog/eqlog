d2b26682b0ca267a13c0b350255669621c7dc1ca
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2022-04-18 00:56:05 +0200

    Cut down grammar

--- src/ast.rs
+++ src/ast.rs
@@ -9,23 +9,6 @@ pub enum Def {
         into_ty: Ty,
         unit_case: Tm,
     },
-    BoolInd {
-        name: String,
-        into_var: String,
-        into_ty: Ty,
-        true_case: Tm,
-        false_case: Tm,
-    },
-    NatInd {
-        name: String,
-        into_var: String,
-        into_ty: Ty,
-        zero_case: Tm,
-        succ_nat_var: String,
-        succ_hyp_var: String,
-        succ_hyp_ty: Ty,
-        succ_tm: Tm,
-    },
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -34,19 +17,12 @@ pub enum Tm {
     App { fun: String, args: Vec<Tm> },
     Let { body: Vec<Def>, result: Box<Tm> },
     UnitTm,
-    True,
-    False,
-    Neg(Box<Tm>),
     Refl(Box<Tm>),
-    Z,
-    S(Box<Tm>),
 }
 
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Ty {
-    Unit,
-    Bool,
-    Nat,
+    UnitTy,
     Eq(Box<Tm>, Box<Tm>),
 }
