b17fe6d05c4c9685dadd92cde105c86981fece6a
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2022-04-18 00:28:03 +0200

    Move grammar into root src dir

--- src/lang/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod ast;
-
-lalrpop_mod!(#[allow(unused_parens, dead_code)] pub parser, "/lang/epa.rs");
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 #![recursion_limit="128"]
 #[macro_use]
-extern crate lalrpop_util;
-#[macro_use]
 extern crate lazy_static;
 #[macro_use] #[cfg(test)]
 extern crate maplit;
 extern crate eqlog_util;
 use eqlog_util::eqlog_mod;
+extern crate lalrpop_util;
+use lalrpop_util::lalrpop_mod;
 
+lalrpop_mod!(#[allow(unused)] grammar);
 eqlog_mod!(cwf_new);
-
+pub mod ast;
 #[macro_use]
 pub mod eqlog;
 pub mod cwf;
-pub mod lang;
 pub mod cwf_checker;
