8f185067ad6f10a945518028b3eb466720d67d9d
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2022-05-17 20:29:31 +0200

    Use define_{func} functions in exp-epa

--- src/cwf_checker.rs
+++ src/cwf_checker.rs
@@ -1,6 +1,6 @@
+use crate::ast;
 use crate::cwf::*;
 use std::collections::HashMap;
-use crate::ast;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Checking {
@@ -35,31 +35,20 @@ impl Scope {
 }
 
 impl Scope {
-    fn ty(&mut self, tm: Tm) -> Ty {
-        let ty = self.cwf.new_ty();
-        self.cwf.insert_tm_ty(TmTy(tm, ty));
-        ty
-    }
     fn add_type(&mut self, checking: Checking, ty: &ast::Ty) -> Ty {
         match ty {
-            ast::Ty::Unit => {
-                let ty = self.cwf.new_ty();
-                self.cwf.insert_unit(Unit(*self.extensions.last().unwrap(), ty));
-                ty
-            }
+            ast::Ty::Unit => self.cwf.define_unit(*self.extensions.last().unwrap()),
             ast::Ty::Eq(lhs, rhs) => {
                 let lhs = self.add_term(checking, lhs);
                 let rhs = self.add_term(checking, rhs);
-                
+
                 if checking == Checking::Yes {
-                    let lhs_ty = self.ty(lhs);
-                    let rhs_ty = self.ty(lhs);
+                    let lhs_ty = self.cwf.define_tm_ty(lhs);
+                    let rhs_ty = self.cwf.define_tm_ty(lhs);
                     self.cwf.close();
                     assert_eq!(self.cwf.ty_root(lhs_ty), self.cwf.ty_root(rhs_ty));
                 }
-                let eq_ty = self.cwf.new_ty();
-                self.cwf.insert_eq(Eq(lhs, rhs, eq_ty));
-                eq_ty
+                self.cwf.define_eq(lhs, rhs)
             }
         }
     }
@@ -69,25 +58,19 @@ impl Scope {
                 let tm = self.add_term(checking, tm);
                 let ty = self.add_type(checking, ty);
                 if checking == Checking::Yes {
-                    let tm_ty = self.ty(tm);
+                    let tm_ty = self.cwf.define_tm_ty(tm);
                     self.cwf.close();
                     assert_eq!(self.cwf.ty_root(tm_ty), self.cwf.ty_root(ty));
                 }
                 tm
             }
-            ast::Tm::App{..} => panic!(),
+            ast::Tm::App { .. } => panic!(),
             ast::Tm::Let { .. } => panic!(),
-            ast::Tm::UnitTm => {
-                let unit = self.cwf.new_tm();
-                self.cwf.insert_unit_tm(UnitTm(*self.extensions.last().unwrap(), unit));
-                unit
-            },
+            ast::Tm::UnitTm => self.cwf.define_unit_tm(*self.extensions.last().unwrap()),
             ast::Tm::Refl(s) => {
                 let s = self.add_term(checking, s);
-                let refl = self.cwf.new_tm();
-                self.cwf.insert_refl(Refl(s, refl));
-                refl
-            },
+                self.cwf.define_refl(s)
+            }
         }
     }
     // Adjoing indeterminate term of a given type, do not change context.
@@ -95,45 +78,50 @@ impl Scope {
         let ty = self.add_type(checking, ty);
         let var = self.cwf.new_tm();
         self.cwf.insert_tm_ty(TmTy(var, ty));
-        self.definitions.insert(name.to_string(), Definition {
-            ambient_ctx: *self.extensions.last().unwrap(),
-            extensions: Vec::new(),
-            tm: var,
-        });
+        self.definitions.insert(
+            name.to_string(),
+            Definition {
+                ambient_ctx: *self.extensions.last().unwrap(),
+                extensions: Vec::new(),
+                tm: var,
+            },
+        );
     }
     // Extend context by a variable.
     fn extend_context(&mut self, checking: Checking, name: &str, ty: &ast::Ty) {
         let ty = self.add_type(checking, ty);
         let base_ctx = *self.extensions.last().unwrap();
-        let ext_ctx = self.cwf.new_ctx();
-        self.cwf.insert_ext_ctx(ExtCtx(base_ctx, ty, ext_ctx));
-        let var = self.cwf.new_tm();
-        self.cwf.insert_var(Var(base_ctx, ty, var));
+        let ext_ctx = self.cwf.define_ext_ctx(base_ctx, ty);
+        let var = self.cwf.define_var(base_ctx, ty);
         self.extensions.push(ext_ctx);
-        self.definitions.insert(name.to_string(), Definition {
-            ambient_ctx: ext_ctx,
-            extensions: Vec::new(),
-            tm: var,
-        });
+        self.definitions.insert(
+            name.to_string(),
+            Definition {
+                ambient_ctx: ext_ctx,
+                extensions: Vec::new(),
+                tm: var,
+            },
+        );
     }
-                    
-    pub fn add_definition(&mut self, checking: Checking, def: &ast::Def) {
-        use ast::Def::*;
-        match def {
-            Dump => {
-                println!("{:?}", self.cwf);
-            }
-            Def { name, args, ty, tm } => {
-                if checking == Checking::Yes {
-                    let mut copy = self.clone();
-                    for (arg_var, arg_ty) in args.iter() {
-                        copy.adjoin_variable(Checking::Yes, arg_var, arg_ty);
-                    }
-                    copy.add_term(Checking::Yes, tm);
-                }
-            }
-            UnitInd { name, var, into_ty, unit_case } => {
-            }
-        }
-    }
+
+    //pub fn add_definition(&mut self, checking: Checking, def: &ast::Def) {
+    //    panic!();
+    //    use ast::Def::*;
+    //    match def {
+    //        Dump => {
+    //            println!("{:?}", self.cwf);
+    //        }
+    //        Def { name, args, ty, tm } => {
+    //            if checking == Checking::Yes {
+    //                let mut copy = self.clone();
+    //                for (arg_var, arg_ty) in args.iter() {
+    //                    copy.adjoin_variable(Checking::Yes, arg_var, arg_ty);
+    //                }
+    //                copy.add_term(Checking::Yes, tm);
+    //            }
+    //        }
+    //        UnitInd { name, var, into_ty, unit_case } => {
+    //        }
+    //    }
+    //}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
-#![recursion_limit="128"]
-#[macro_use] #[cfg(test)]
+#![recursion_limit = "128"]
+#[macro_use]
+#[cfg(test)]
 extern crate maplit;
 extern crate eqlog_util;
 use eqlog_util::eqlog_mod;
 extern crate lalrpop_util;
 use lalrpop_util::lalrpop_mod;
 
-lalrpop_mod!(#[allow(unused)] grammar);
+lalrpop_mod!(
+    #[allow(unused)]
+    grammar
+);
 eqlog_mod!(cwf);
 pub mod ast;
 
