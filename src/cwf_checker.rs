bfa5feffc4955a268617959d4970e74b3d655345
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2022-05-24 16:10:09 +0200

    Implement defs, apps, add tests

--- src/cwf_checker.rs
+++ src/cwf_checker.rs
@@ -24,7 +24,7 @@ pub struct Scope {
 }
 
 impl Scope {
-    fn new() -> Self {
+    pub fn new() -> Self {
         let mut cwf = Cwf::new();
         let empty_context = cwf.new_ctx();
         Scope {
@@ -42,6 +42,50 @@ impl Scope {
             .last()
             .map(|(_, ctx)| *ctx)
             .unwrap_or(self.empty_context)
+    }
+
+    fn add_weakening_from_base(&mut self, def: &Definition) -> Mor {
+        let base_to_current_exts: &[(Ty, Ctx)] = match self
+            .extensions
+            .iter()
+            .position(|(_, ctx)| *ctx == def.base_context)
+        {
+            None => {
+                debug_assert_eq!(self.empty_context, def.base_context);
+                self.extensions.as_slice()
+            }
+            Some(i) => &self.extensions[(i + 1)..],
+        };
+
+        let mut ctx = def.base_context;
+        let mut w = self.cwf.define_id(ctx);
+        for (ty, ext_ctx) in base_to_current_exts.iter().copied() {
+            let wkn = self.cwf.define_wkn(ctx, ty);
+            w = self.cwf.define_comp(wkn, w);
+            ctx = ext_ctx;
+        }
+        w
+    }
+
+    fn add_substitution(&mut self, checking: Checking, def: &Definition, args: &[Tm]) -> Mor {
+        if checking == Checking::Yes {
+            assert_eq!(args.len(), def.extensions.len())
+        }
+        let mut subst = self.add_weakening_from_base(def);
+        let mut subst_dom = def.base_context;
+        for (arg, (ty, ext_ctx)) in args.iter().copied().zip(def.extensions.iter().copied()) {
+            let subst_ty = self.cwf.define_subst_ty(subst, ty);
+            if checking == Checking::Yes {
+                let arg_ty = self.cwf.define_tm_ty(arg);
+                self.cwf.close();
+                assert_eq!(self.cwf.ty_root(subst_ty), self.cwf.ty_root(arg_ty));
+            } else {
+                self.cwf.insert_tm_ty(TmTy(arg, subst_ty));
+            }
+            subst = self.cwf.define_mor_ext(subst_dom, ty, subst, arg);
+            subst_dom = ext_ctx;
+        }
+        subst
     }
 
     fn add_type(&mut self, checking: Checking, ty: &ast::Ty) -> Ty {
@@ -63,6 +107,11 @@ impl Scope {
     }
     fn add_term(&mut self, checking: Checking, tm: &ast::Tm) -> Tm {
         match tm {
+            ast::Tm::Variable(name) => {
+                let def = self.definitions.get(name).unwrap().clone();
+                let wkn = self.add_weakening_from_base(&def);
+                self.cwf.define_subst_tm(wkn, def.term)
+            }
             ast::Tm::Typed { tm, ty } => {
                 let tm = self.add_term(checking, tm);
                 let ty = self.add_type(checking, ty);
@@ -74,7 +123,13 @@ impl Scope {
                 tm
             }
             ast::Tm::App { fun, args } => {
-                panic!()
+                let def = self.definitions.get(fun).unwrap().clone();
+                let args: Vec<Tm> = args
+                    .iter()
+                    .map(|arg| self.add_term(checking, arg))
+                    .collect();
+                let subst = self.add_substitution(checking, &def, args.as_slice());
+                self.cwf.define_subst_tm(subst, def.term)
             }
             ast::Tm::Let { body, result } => {
                 let before_defs = self.definitions.clone();
