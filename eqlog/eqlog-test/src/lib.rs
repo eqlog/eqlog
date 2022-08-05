fe4e11e1d6c0381875b79ae62feb86f9107647d7
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2022-08-05 23:15:57 +0200

    Add test for transitive hull

--- eqlog/eqlog-test/src/lib.rs
+++ eqlog/eqlog-test/src/lib.rs
@@ -4,9 +4,12 @@ eqlog_mod!(monoid);
 eqlog_mod!(pointed);
 eqlog_mod!(trivial_idempotent);
 eqlog_mod!(logic);
+eqlog_mod!(trans_refl);
 
 mod category_mod;
 mod monoid_test;
 mod pointed_test;
 #[cfg(test)]
 mod logic_test;
+#[cfg(test)]
+mod trans_refl_test;
