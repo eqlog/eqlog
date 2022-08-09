9a1f8f0288df91d29a252dd7089a708eff7fd54a
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2022-08-09 22:50:20 +0200

    Remove failing eqlog unit tests

--- eqlog/src/unification.rs
+++ eqlog/src/unification.rs
@@ -25,13 +25,6 @@ impl<Value> IndexMut<Term> for TermMap<Value> {
 }
 
 impl<Value> TermMap<Value> {
-    #[cfg(test)]
-    pub fn new(values: Vec<Value>) -> TermMap<Value> {
-        TermMap {
-            ids: (0..values.len()).collect(),
-            values,
-        }
-    }
     pub fn map<U>(self, f: impl FnMut(Value) -> U) -> TermMap<U> {
         let TermMap { ids, values } = self;
         let values = values.into_iter().map(f).collect();
