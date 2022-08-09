3287601a231ab92af5ece81d883f756cb1251a53
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2022-08-09 19:30:35 +0200

    Add more tests about finite products

--- eqlog/eqlog-test/src/product_category_test.rs
+++ eqlog/eqlog-test/src/product_category_test.rs
@@ -18,5 +18,57 @@ fn test_products_are_isomorphic() {
     // that they exist. This means that the generated category is finite here, and we can close the
     // model without exit condition.
     cat.close();
-    assert!(cat.is_iso(cat.obj_root(p1_obj), cat.obj_root(p2_obj)));
+    assert!(cat.is_iso(p1_obj, p2_obj));
+}
+
+#[test]
+fn test_products_are_commutative() {
+    let mut cat = ProductCategory::new();
+    let x = cat.new_obj();
+    let y = cat.new_obj();
+
+    let xy = cat.new_prod();
+    cat.insert_left(Left(xy, x));
+    cat.insert_right(Right(xy, y));
+
+    let yx = cat.new_prod();
+    cat.insert_left(Left(yx, y));
+    cat.insert_right(Right(yx, x));
+
+    let xy_obj = cat.define_prod_obj(xy);
+    let yx_obj = cat.define_prod_obj(yx);
+
+    cat.close();
+    assert!(cat.is_iso(xy_obj, yx_obj));
+}
+
+#[test]
+fn test_products_are_associative() {
+    let mut cat = ProductCategory::new();
+    let x = cat.new_obj();
+    let y = cat.new_obj();
+    let z = cat.new_obj();
+
+    let xy = cat.new_prod();
+    cat.insert_left(Left(xy, x));
+    cat.insert_right(Right(xy, y));
+    let xy_obj = cat.define_prod_obj(xy);
+
+    let xy_z = cat.new_prod();
+    cat.insert_left(Left(xy_z, xy_obj));
+    cat.insert_right(Right(xy_z, z));
+    let xy_z_obj = cat.define_prod_obj(xy_z);
+
+    let yz = cat.new_prod();
+    cat.insert_left(Left(yz, y));
+    cat.insert_right(Right(yz, z));
+    let yz_obj = cat.define_prod_obj(yz);
+
+    let x_yz = cat.new_prod();
+    cat.insert_left(Left(x_yz, x));
+    cat.insert_right(Right(x_yz, yz_obj));
+    let x_yz_obj = cat.define_prod_obj(x_yz);
+
+    cat.close();
+    assert!(cat.is_iso(xy_z_obj, x_yz_obj));
 }
