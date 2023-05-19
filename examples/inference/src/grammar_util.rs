fee49732cb6559a0c3888ef0e5097776054c8b82
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2023-05-19 14:47:14 +0200

    Start refactoring to get rid of explicit AST
    This commit includes most of the parser, but semantics are still
    missing.

---
+++ examples/inference/src/grammar_util.rs
@@ -0,0 +1,58 @@
+use crate::program::*;
+
+use std::collections::HashMap;
+
+pub struct Literals {
+    pub vars: HashMap<String, Var>,
+    pub strings: HashMap<String, StringLiteral>,
+    pub numbers: HashMap<String, NumberLiteral>,
+}
+
+impl Literals {
+    pub fn new() -> Literals {
+        Literals {
+            vars: HashMap::new(),
+            strings: HashMap::new(),
+            numbers: HashMap::new(),
+        }
+    }
+}
+
+pub fn type_node_list(nodes: &[TypeNode], p: &mut Program) -> TypeNodeList {
+    let mut l = p.define_nil_type_node_list();
+    for node in nodes {
+        l = p.define_cons_type_node_list(*node, l);
+    }
+    l
+}
+
+pub fn expr_node_list(nodes: &[ExprNode], p: &mut Program) -> ExprNodeList {
+    let mut l = p.define_nil_expr_node_list();
+    for node in nodes {
+        l = p.define_cons_expr_node_list(*node, l);
+    }
+    l
+}
+
+pub fn stmt_node_list(nodes: &[StmtNode], p: &mut Program) -> StmtNodeList {
+    let mut l = p.define_nil_stmt_node_list();
+    for node in nodes {
+        l = p.define_cons_stmt_node_list(*node, l);
+    }
+    l
+}
+
+pub fn type_node_opt(node: Option<TypeNode>, p: &mut Program) -> TypeNodeOpt {
+    match node {
+        Some(node) => p.define_some_type_node_opt(node),
+        None => p.define_none_type_node_opt(),
+    }
+}
+
+pub fn arg_list(args: &[(Var, TypeNodeOpt)], p: &mut Program) -> ArgList {
+    let mut l = p.define_nil_arg_list();
+    for (var, ty) in args {
+        l = p.define_cons_arg_list(*var, *ty, l);
+    }
+    l
+}
--- examples/inference/src/main.rs
+++ examples/inference/src/main.rs
@@ -1,16 +1,15 @@
-mod ast;
-use lalrpop_util::lalrpop_mod;
-lalrpop_mod!(grammar);
 use eqlog_runtime::eqlog_mod;
-eqlog_mod!(type_system);
-mod type_check;
+use lalrpop_util::lalrpop_mod;
+eqlog_mod!(program);
+mod grammar_util;
+lalrpop_mod!(grammar);
 
 use crate::grammar::ProgramParser;
-use crate::type_system::TypeSystem;
+use crate::grammar_util::Literals;
+use crate::program::*;
 use std::env;
 use std::fs;
 use std::process::ExitCode;
-use type_check::*;
 
 fn main() -> ExitCode {
     let mut args = env::args();
@@ -32,7 +31,10 @@ fn main() -> ExitCode {
         }
     };
 
-    let stmts: Vec<ast::Stmt> = match ProgramParser::new().parse(&contents) {
+    let mut p = Program::new();
+    let mut literals = Literals::new();
+
+    let stmts: StmtNodeList = match ProgramParser::new().parse(&mut p, &mut literals, &contents) {
         Ok(stmts) => stmts,
         Err(err) => {
             eprintln!("Syntax error: {err}");
@@ -40,37 +42,37 @@ fn main() -> ExitCode {
         }
     };
 
-    let mut ts = TypeSystem::new();
-    let mut bindings: Bindings = Bindings::new();
-    populate_stmts(&mut ts, &mut bindings, stmts.as_slice());
+    //let mut ts = TypeSystem::new();
+    //let mut bindings: Bindings = Bindings::new();
+    //populate_stmts(&mut ts, &mut bindings, stmts.as_slice());
 
-    ts.close();
+    //ts.close();
 
-    assert!(!ts.conflict());
+    //assert!(!ts.conflict());
 
-    let k: type_system::Expr = ts.variable_expr(*bindings.get("k").unwrap()).unwrap();
-    let number: type_system::Ty = ts.number_ty().unwrap();
-    assert!(ts.are_equal_ty(ts.expr_ty(k).unwrap(), number));
+    //let k: type_system::Expr = ts.variable_expr(*bindings.get("k").unwrap()).unwrap();
+    //let number: type_system::Ty = ts.number_ty().unwrap();
+    //assert!(ts.are_equal_ty(ts.expr_ty(k).unwrap(), number));
 
-    let asdf: type_system::Expr = ts.variable_expr(*bindings.get("asdf").unwrap()).unwrap();
-    let asdf_ty: type_system::Ty = ts.expr_ty(asdf).unwrap();
+    //let asdf: type_system::Expr = ts.variable_expr(*bindings.get("asdf").unwrap()).unwrap();
+    //let asdf_ty: type_system::Ty = ts.expr_ty(asdf).unwrap();
 
-    let asdf_ty_dom: type_system::TyList = ts.domain(asdf_ty).unwrap();
-    let nil_tys = ts.nil_ty_list().unwrap();
-    assert!(ts.are_equal_ty_list(asdf_ty_dom, nil_tys));
+    //let asdf_ty_dom: type_system::TyList = ts.domain(asdf_ty).unwrap();
+    //let nil_tys = ts.nil_ty_list().unwrap();
+    //assert!(ts.are_equal_ty_list(asdf_ty_dom, nil_tys));
 
-    let asdf_ty_cod: type_system::Ty = ts.codomain(asdf_ty).unwrap();
-    let string: type_system::Ty = ts.string_ty().unwrap();
-    assert!(ts.are_equal_ty(asdf_ty_cod, string));
+    //let asdf_ty_cod: type_system::Ty = ts.codomain(asdf_ty).unwrap();
+    //let string: type_system::Ty = ts.string_ty().unwrap();
+    //assert!(ts.are_equal_ty(asdf_ty_cod, string));
 
-    let void_func: type_system::Expr = ts
-        .variable_expr(*bindings.get("void_func").unwrap())
-        .unwrap();
-    let void_func_ty: type_system::Ty = ts.expr_ty(void_func).unwrap();
+    //let void_func: type_system::Expr = ts
+    //    .variable_expr(*bindings.get("void_func").unwrap())
+    //    .unwrap();
+    //let void_func_ty: type_system::Ty = ts.expr_ty(void_func).unwrap();
 
-    let void_func_ty_cod: type_system::Ty = ts.codomain(void_func_ty).unwrap();
-    let void_ty: type_system::Ty = ts.void_ty().unwrap();
-    assert!(ts.are_equal_ty(void_func_ty_cod, void_ty));
+    //let void_func_ty_cod: type_system::Ty = ts.codomain(void_func_ty).unwrap();
+    //let void_ty: type_system::Ty = ts.void_ty().unwrap();
+    //assert!(ts.are_equal_ty(void_func_ty_cod, void_ty));
 
     ExitCode::SUCCESS
 }
