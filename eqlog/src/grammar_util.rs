087f955e905ad3bb2c601c2825f694af4303cb04
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2023-09-28 11:50:36 +0200

    Put type/pred/func decl data into eqlog model

--- eqlog/src/grammar_util.rs
+++ eqlog/src/grammar_util.rs
@@ -36,6 +36,17 @@ pub fn decl_list_node(nodes: &[DeclNode], eqlog: &mut Eqlog) -> DeclListNode {
     l
 }
 
+pub fn arg_decl_list_node(nodes: &[ArgDeclNode], eqlog: &mut Eqlog) -> ArgDeclListNode {
+    let mut l = eqlog.new_arg_decl_list_node();
+    eqlog.insert_nil_arg_decl_list_node(l);
+    for node in nodes.iter().rev() {
+        let cons = eqlog.new_arg_decl_list_node();
+        eqlog.insert_cons_arg_decl_list_node(cons, *node, l);
+        l = cons;
+    }
+    l
+}
+
 pub fn opt_term_node(o: Option<TermNode>, eqlog: &mut Eqlog) -> OptTermNode {
     let opt_node = eqlog.new_opt_term_node();
     match o {
