f41c5fe3cf77858ac5c95a701b93131a4aa1299e
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2023-10-14 14:15:58 +0200

    Migrate poset test to standalone crate

--- eqlog-test/src/lib.rs
+++ eqlog-test/src/lib.rs
@@ -1,13 +1,10 @@
 use eqlog_runtime::eqlog_mod;
 eqlog_mod!(trivial_idempotent);
 eqlog_mod!(trans_refl);
-eqlog_mod!(poset);
 eqlog_mod!(semilattice);
 eqlog_mod!(product_category);
 eqlog_mod!(reduction_from_nullary);
 
-#[cfg(test)]
-mod poset_test;
 #[cfg(test)]
 mod product_category_test;
 #[cfg(test)]
--- test-crates/poset/src/lib.rs
+++ test-crates/poset/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use eqlog_runtime::eqlog_mod;
+eqlog_mod!(poset);
+pub use crate::poset::*;
+
+pub fn adjoin_chain(p: &mut Poset, length: usize) -> (P, P) {
+    assert_ne!(length, 0);
+
+    let first = p.new_p();
+    let mut prev = first;
+    for _ in 0..(length - 1) {
+        let next = p.new_p();
+        p.insert_le(prev, next);
+        prev = next;
+    }
+    let last = prev;
+    (first, last)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[test]
+fn test_collapse() {
+    let mut p = Poset::new();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let (first0, last0) = adjoin_chain(&mut p, 1);
+    adjoin_chain(&mut p, 7);
+    let (first2, last2) = adjoin_chain(&mut p, 20);
+
+    // Link chains 0 and 2, forming a single loop.
+    p.insert_le(last0, first2);
+    p.insert_le(last2, first0);
+
+    p.close();
+    assert_eq!(p.iter_p().count(), 8);
+    assert_eq!(p.iter_le().count(), 1 + (7 * 8 / 2));
 }
