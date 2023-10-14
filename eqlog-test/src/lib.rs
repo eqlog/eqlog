72fbcbf2ee832940f287bfde9f2f76eaa196c47b
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2023-10-14 14:14:31 +0200

    Migrate pointed test to standalone crate

--- eqlog-test/src/lib.rs
+++ eqlog-test/src/lib.rs
@@ -1,5 +1,4 @@
 use eqlog_runtime::eqlog_mod;
-eqlog_mod!(pointed);
 eqlog_mod!(trivial_idempotent);
 eqlog_mod!(trans_refl);
 eqlog_mod!(poset);
@@ -7,7 +6,6 @@ eqlog_mod!(semilattice);
 eqlog_mod!(product_category);
 eqlog_mod!(reduction_from_nullary);
 
-mod pointed_test;
 #[cfg(test)]
 mod poset_test;
 #[cfg(test)]
