4b05aedb85671a5712cef62050f258055338ef64
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2023-10-14 14:20:41 +0200

    Migrate semilattice test to standalone crate

--- eqlog-test/src/lib.rs
+++ eqlog-test/src/lib.rs
@@ -1,9 +1,6 @@
 use eqlog_runtime::eqlog_mod;
 eqlog_mod!(trivial_idempotent);
 eqlog_mod!(trans_refl);
-eqlog_mod!(semilattice);
 
-#[cfg(test)]
-mod semilattice_test;
 #[cfg(test)]
 mod trans_refl_test;
--- test-crates/semilattice/src/lib.rs
+++ test-crates/semilattice/src/lib.rs
@@ -1,14 +1,17 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use eqlog_runtime::eqlog_mod;
+eqlog_mod!(semilattice);
+pub use crate::semilattice::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[test]
+fn test_semilattice_associative() {
+    let mut sl = Semilattice::new();
+    let x = sl.new_el();
+    let y = sl.new_el();
+    let z = sl.new_el();
+    sl.close();
+    let xy = sl.meet(x, y).unwrap();
+    let xy_z = sl.meet(xy, z).unwrap();
+    let yz = sl.meet(y, z).unwrap();
+    let x_yz = sl.meet(x, yz).unwrap();
+    assert!(sl.are_equal_el(xy_z, x_yz));
 }
