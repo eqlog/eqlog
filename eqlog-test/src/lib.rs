d42c6a35ce0f7a82a2599e14a3aba52e3f62f903
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2023-10-14 14:18:02 +0200

    Migrate product category test to standalone crate

--- eqlog-test/src/lib.rs
+++ eqlog-test/src/lib.rs
@@ -2,11 +2,8 @@ use eqlog_runtime::eqlog_mod;
 eqlog_mod!(trivial_idempotent);
 eqlog_mod!(trans_refl);
 eqlog_mod!(semilattice);
-eqlog_mod!(product_category);
 eqlog_mod!(reduction_from_nullary);
 
-#[cfg(test)]
-mod product_category_test;
 #[cfg(test)]
 mod semilattice_test;
 #[cfg(test)]
