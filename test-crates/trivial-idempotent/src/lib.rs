2c82166aaf481ea124f5bec94a390ffdc107ec80
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2023-10-14 14:05:56 +0200

    Migrate trivial-idempotent test to standalone crate

--- test-crates/trivial-idempotent/src/lib.rs
+++ test-crates/trivial-idempotent/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use eqlog_runtime::eqlog_mod;
+eqlog_mod!(trivial_idempotent);
+pub use crate::trivial_idempotent::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[test]
+fn trivial_idempotent() {
+    use crate::trivial_idempotent::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let mut m = TrivialIdempotent::new();
+
+    let el0 = m.new_m();
+    m.insert_e(el0);
+    let el1 = m.new_m();
+    m.insert_mul(el1, el1, el1);
+
+    m.close();
+
+    assert!(m.are_equal_m(el0, el1));
+    assert_eq!(m.iter_m().count(), 1);
 }
