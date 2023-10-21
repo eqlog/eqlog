42ca6dfcfa53410d98aef9a45be49db69dd967ab
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2023-10-21 19:36:33 +0200

    Check in prebuilt version for eqlog-eqlog
    This also bumps the version of eqlog used for building eqlog-eqlog to
    0.3.0, which was necessary because the `process` instead of
    `process_root` was added in 0.3.0.

--- eqlog-eqlog/build.rs
+++ eqlog-eqlog/build.rs
@@ -1,3 +1,28 @@
+#[cfg(feature = "rebuild")]
+fn main() -> std::process::ExitCode {
+    use std::path::{Path, PathBuf};
+    use std::process::ExitCode;
+
+    let prebuilt_path = Path::new("prebuilt").canonicalize().unwrap();
+    let config = eqlog::Config {
+        in_dir: PathBuf::from("src"),
+        out_dir: prebuilt_path,
+    };
+
+    if let Err(err) = eqlog::process(&config) {
+        eprintln!("{err}");
+        return ExitCode::FAILURE
+    }
+
+    println!("cargo:rustc-env=EQLOG_OUT_DIR={}", config.out_dir.display());
+    ExitCode::SUCCESS
+}
+
+
+#[cfg(not(feature = "rebuild"))]
 fn main() {
-    eqlog::process_root();
+    use std::path::Path;
+
+    let prebuilt_path = Path::new("prebuilt").canonicalize().unwrap();
+    println!("cargo:rustc-env=EQLOG_OUT_DIR={}", prebuilt_path.display());
 }
