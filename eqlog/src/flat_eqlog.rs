6431be7ed91be4dca74ce9dbdbaa73a1dff2e760
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2023-11-04 08:53:34 +0100

    Add function to assign flat vars to chain elements

--- eqlog/src/flat_eqlog.rs
+++ eqlog/src/flat_eqlog.rs
@@ -2,58 +2,69 @@
 
 use eqlog_eqlog::*;
 
+#[derive(Copy, Clone, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
 pub struct FlatVar(pub usize);
 
+#[derive(Clone, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
 pub struct FlatStmtEqual {
     pub lhs: FlatVar,
     pub rhs: FlatVar,
     pub typ: Type,
 }
 
+#[derive(Clone, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
 pub enum Rel {
     Pred(Pred),
     Func(Func),
 }
 
+#[derive(Clone, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
 pub struct FlatIfRelArg {
     pub var: FlatVar,
     pub typ: Type,
     pub fresh: bool,
 }
 
+#[derive(Clone, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
 pub struct FlatIfStmtRelation {
     pub rel: Rel,
     pub args: Vec<FlatIfRelArg>,
     pub only_dirty: bool,
 }
 
+#[derive(Clone, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
 pub struct FlatIfStmtType {
     pub var: FlatVar,
     pub typ: Type,
     pub only_dirty: bool,
 }
 
+#[derive(Clone, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
 pub enum FlatIfStmt {
     Equal(FlatStmtEqual),
     Relation(FlatIfStmtRelation),
     Type(FlatIfStmtType),
 }
 
+#[derive(Clone, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
 pub struct FlatThenRelArg {
     pub var: FlatVar,
     pub typ: Type,
 }
 
+#[derive(Clone, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
 pub struct FlatThenStmtRelation {
     pub rel: Rel,
     pub args: Vec<FlatThenRelArg>,
 }
 
+#[derive(Clone, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
 pub struct FlatThenStmtType {
     pub var: FlatVar,
     pub typ: Type,
 }
 
+#[derive(Clone, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
 pub enum FlatSurjThenStmt {
     Equal(FlatStmtEqual),
     Relation(FlatThenStmtRelation),
@@ -62,6 +73,7 @@ pub enum FlatSurjThenStmt {
 
 pub type FlatNonSurjThenStmt = FlatThenStmtRelation;
 
+#[derive(Clone, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
 pub enum FlatStmt {
     If(FlatIfStmt),
     SurjThen(FlatSurjThenStmt),
