854d9bd2c878f805510f57a4c4886b4a8a0ee669
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2023-11-08 14:42:06 +0100

    Add eqlog binary

---
+++ eqlog-tree/src/main.rs
@@ -0,0 +1,1 @@
+../../eqlog/src/main.rs
---
+++ eqlog/src/main.rs
@@ -0,0 +1,28 @@
+use clap::Parser;
+use eqlog::{process, Config};
+use std::path::PathBuf;
+use std::process::ExitCode;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    src: PathBuf,
+    out: PathBuf,
+}
+
+fn main() -> ExitCode {
+    let Cli { src, out } = Cli::parse();
+    let config = Config {
+        in_dir: src,
+        out_dir: out,
+    };
+    match process(&config) {
+        Ok(()) => (),
+        Err(err) => {
+            eprintln!("{err}");
+            return ExitCode::FAILURE;
+        }
+    }
+
+    ExitCode::SUCCESS
+}
