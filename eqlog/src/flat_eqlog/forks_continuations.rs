336ff3ef53063e45e7e7355a5878130101820b23
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2023-11-14 11:14:45 +0100

    Fix continuation computation

--- eqlog/src/flat_eqlog/forks_continuations.rs
+++ eqlog/src/flat_eqlog/forks_continuations.rs
@@ -12,7 +12,6 @@ fn forks_continuations_rec<'a>(
     stmts: &'a [FlatStmt],
     forks: &mut Vec<ForkSuffix<'a>>,
     continuations: &mut BTreeMap<ByAddress<&'a [FlatStmt]>, usize>,
-    continuation: Option<usize>,
 ) {
     for (stmt_index, stmt) in stmts.iter().enumerate() {
         let fork_stmt = match stmt {
@@ -23,15 +22,13 @@ fn forks_continuations_rec<'a>(
         };
 
         let suffix = &stmts[(stmt_index + 1)..];
+        let continuation = forks.len();
         forks.push(ForkSuffix { fork_stmt, suffix });
-        if let Some(continuation) = continuation {
-            continuations.insert(ByAddress(&suffix[suffix.len()..]), continuation);
-        }
-
-        let continuation = forks.len() - 1;
 
         for block in fork_stmt.blocks.iter() {
-            forks_continuations_rec(block.as_slice(), forks, continuations, Some(continuation));
+            let empty_block_suffix = &block[block.len()..];
+            continuations.insert(ByAddress(empty_block_suffix), continuation);
+            forks_continuations_rec(block.as_slice(), forks, continuations);
         }
     }
 }
@@ -44,12 +41,10 @@ pub fn forks_continuations<'a>(
 ) {
     let mut fork_suffixes = Vec::new();
     let mut fork_continuations = BTreeMap::new();
-    let initial_continuation = None;
     forks_continuations_rec(
         rule.stmts.as_slice(),
         &mut fork_suffixes,
         &mut fork_continuations,
-        initial_continuation,
     );
     (fork_suffixes, fork_continuations)
 }
