e86567b99670fd358266a6d792516748d19a94e8
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2023-11-15 20:18:55 +0100

    Make eqlog-tree binary compile
    This required copying the main.rs file unfortunately, since the eqlog
    import needed to be changed to eqlog_tree.

--- eqlog-tree/src/main.rs
+++ eqlog-tree/src/main.rs
@@ -1,1 +1,28 @@
-../../eqlog/src/main.rs
+use clap::Parser;
+use eqlog_tree::{process, Config};
+use std::path::PathBuf;
+use std::process::ExitCode;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    src: PathBuf,
+    out: PathBuf,
+}
+
+fn main() -> ExitCode {
+    let Cli { src, out } = Cli::parse();
+    let config = Config {
+        in_dir: src,
+        out_dir: out,
+    };
+    match process(&config) {
+        Ok(()) => (),
+        Err(err) => {
+            eprintln!("{err}");
+            return ExitCode::FAILURE;
+        }
+    }
+
+    ExitCode::SUCCESS
+}
