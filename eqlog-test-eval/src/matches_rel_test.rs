b869bf434830728ac5289edaffba39927db7afb0
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2024-09-19 16:59:32 +0200

    Make matches_rel_tests active, add more tests there
    The module wasn't declared in lib.rs, so its tests weren't executed.

--- eqlog-test-eval/src/matches_rel_test.rs
+++ eqlog-test-eval/src/matches_rel_test.rs
@@ -5,17 +5,45 @@ use crate::matches_rel::*;
 fn different_pred_func_works() {
     let mut matches_rel = MatchesRel::new();
 
-    let pred = matches.new_pred();
-    let func = matches.new_func();
+    let pred = matches_rel.new_pred();
+    let func = matches_rel.new_func();
 
-    let pred_rel = matches.define_pred_rel(pred);
-    let func_rel = matches.define_func_rel(func);
+    let pred_rel = matches_rel.define_pred_rel(pred);
+    let func_rel = matches_rel.define_func_rel(func);
 
-    matches.close();
+    matches_rel.close();
 
     assert!(matches_rel.is_pred(pred_rel));
     assert!(!matches_rel.is_pred(func_rel));
 
     assert!(!matches_rel.is_func(pred_rel));
     assert!(matches_rel.is_func(func_rel));
+}
+
+#[test]
+fn func_only_works() {
+    let mut matches_rel = MatchesRel::new();
+
+    let func = matches_rel.new_func();
+
+    let func_rel = matches_rel.define_func_rel(func);
+
+    matches_rel.close();
+
+    assert!(!matches_rel.is_pred(func_rel));
+    assert!(matches_rel.is_func(func_rel));
+}
+
+#[test]
+fn pred_only_works() {
+    let mut matches_rel = MatchesRel::new();
+
+    let pred = matches_rel.new_pred();
+
+    let pred_rel = matches_rel.define_pred_rel(pred);
+
+    matches_rel.close();
+
+    assert!(matches_rel.is_pred(pred_rel));
+    assert!(!matches_rel.is_func(pred_rel));
 }
