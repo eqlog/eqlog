d438f0edb6ea6b25e4c77af503d0d39c95b8b6a9
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2025-06-05 15:32:18 +0200

    Recover eqlog_mod! macro in eqlog-runtime
    Was removed a few commits back.

--- eqlog-runtime/src/lib.rs
+++ eqlog-runtime/src/lib.rs
@@ -186,3 +186,34 @@ pub mod btree_set;
 mod types;
 #[doc(hidden)]
 pub mod unification;
+
+/// Declare an eqlog module.
+///
+/// # Examples
+///
+/// ```ignore
+/// use eqlog_runtime::eqlog_mod;
+/// eqlog_mod!(foo);
+/// ```
+///
+/// Eqlog modules can be annotated with a visibility, or with attributes:
+/// ```ignore
+/// eqlog_mod!(#[cfg(test)] pub foo);
+/// ```
+#[macro_export]
+macro_rules! eqlog_mod {
+    ($(#[$attr:meta])* $vis:vis $modname:ident) => {
+        $(#[$attr])* $vis mod $modname {
+            include!(concat!(
+                env!("EQLOG_OUT_DIR"),
+                "/",
+                file!(),
+                "/",
+                "..",
+                "/",
+                stringify!($modname),
+                ".eql.rs"
+            ));
+        }
+    };
+}
