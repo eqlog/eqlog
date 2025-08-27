206282498881aa4605f32001538f9c11500f971c
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2025-08-27 19:03:00 +0200

    Add subset morphism propagate test via API
    Like the one in subset_morphism_test.rs, but the morphism is created via
    the API instead of as part of the theory.

--- eqlog-test-eval/src/subset_test.rs
+++ eqlog-test-eval/src/subset_test.rs
@@ -1,1 +1,19 @@
+use crate::subset::*;
 
+#[test]
+fn test_singleton_subset_propagates() {
+    let mut model = Subset::new();
+
+    let a = model.new_subs();
+    let b = model.new_subs();
+    let mor = model.new_subs_mor();
+    model.insert_subs_mor_dom(mor, a);
+    model.insert_subs_mor_cod(mor, b);
+
+    let el = model.new_carrier();
+    model.insert_element(a, el);
+
+    model.close();
+
+    assert!(model.element(b, el));
+}
